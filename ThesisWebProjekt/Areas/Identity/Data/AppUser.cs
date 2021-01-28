fd80c239f2fdf4456c280e6f38845a8e2c5ba18e
judith.weigand@web.de <1Mnbvcxylkjhgfdsa>
2021-01-28 17:38:08 +0100

    create User Teil1 (funktioniert noch nicht)

--- ThesisWebProjekt/Areas/Identity/Data/AppUser.cs
+++ ThesisWebProjekt/Areas/Identity/Data/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -12,9 +13,17 @@ namespace ThesisWebProjekt.Areas.Identity.Data
     {
         public int Id { get; set; }
 
-// bei 1:1 steht das drin, bei 1:n nicht zwingend nötig
-//        public Lehrstuhl Lehrstuhl { get; set; }
+        [Required]
+        public string Name { get; set; }
 
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public Lehrstuhl Lehrstuhl { get; set; }
         public IEnumerable<Thesis> Theses { get; set; }
     }
 }
---
+++ ThesisWebProjekt/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThesisWebProjekt.Areas.Identity.Data;
+
+namespace ThesisWebProjekt.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AppUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }
+    }
+}
