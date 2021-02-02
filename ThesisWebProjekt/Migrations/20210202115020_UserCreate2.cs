06dc0b2279dde8980fbc1f10cad3b9657d0b4086
judith.weigand@web.de <1Mnbvcxylkjhgfdsa>
2021-02-02 14:04:34 +0100

    DropDownListe fehlt, Fehler UserName bei Create User

---
+++ ThesisWebProjekt/Migrations/20210202115020_UserCreate2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThesisWebProjekt.Migrations
+{
+    public partial class UserCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
