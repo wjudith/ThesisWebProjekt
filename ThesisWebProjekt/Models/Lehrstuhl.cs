6bb2a372cfab2d86bad1aa53ae1d234cffef2a2c
Jenny Lindner <jenny.lindner97@gmail.com>
2021-02-08 16:51:55 +0100

    Version 0

--- ThesisWebProjekt/Models/Lehrstuhl.cs
+++ ThesisWebProjekt/Models/Lehrstuhl.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ThesisWebProjekt.Areas.Identity.Data;
 
 namespace ThesisWebProjekt.Models
 {
-    public partial class Programme
+    public class Lehrstuhl
     {
-        
-        public Programme()
-        {
-            Thesis = new HashSet<Thesis>();
+        public Lehrstuhl() {
+            
         }
-
         public int Id { get; set; }
         [Required]
         [Display(Name = "Name")]
         public string Name { get; set; }
 
-        public ICollection<Thesis> Thesis { get; set; }
+        // public ICollection<AppUser> AppUsers { get; set; }
+
+        public Lehrstuhl Lehrstuhl { get; set; }
+
     }
 }
