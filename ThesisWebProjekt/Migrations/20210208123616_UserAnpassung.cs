7b8787bcdf5f3f7bc733f309046ee623527d51bd
judith.weigand@web.de <1Mnbvcxylkjhgfdsa>
2021-02-08 14:15:09 +0100

    Anpassungen für Registierung

---
+++ ThesisWebProjekt/Migrations/20210208123616_UserAnpassung.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThesisWebProjekt.Migrations
+{
+    public partial class UserAnpassung : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
--- ThesisWebProjekt/Models/AppUser.cs
+++ ThesisWebProjekt/Models/AppUser.cs
@@ -11,17 +11,17 @@ namespace ThesisWebProjekt.Models
     // Add profile data for application users by adding properties to the AppUser class
     public class AppUser : IdentityUser
     {
-        public int Id { get; set; }
+        /*       public int Id { get; set; }
 
-        [Required]
-        public string UserName { get; set; }
+               [Required]
+               public string UserName { get; set; }
 
-        [Required]
-        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
-        public string Email { get; set; }
+               [Required]
+               [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+               public string Email { get; set; } 
 
-        [Required]
-        public string Password { get; set; }
+        [Required] */
+        public string Password { get; set; } 
 
         public Lehrstuhl Lehrstuhl { get; set; }
         public IEnumerable<Thesis> Theses { get; set; }
