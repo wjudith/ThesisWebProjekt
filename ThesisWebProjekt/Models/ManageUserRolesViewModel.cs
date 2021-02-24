177f643c65bf39682553dee63a80bda7e91bc10d
judith.weigand@web.de <1Mnbvcxylkjhgfdsa>
2021-02-24 13:27:02 +0100

    Verknüpfung Rollen-User

---
+++ ThesisWebProjekt/Models/ManageUserRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThesisWebProjekt.Models
+{
+    public class ManageUserRolesViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ ThesisWebProjekt/Models/UserRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThesisWebProjekt.Models
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
